0f742d14a984f2193d008578c23f5e9de68a8362
uYasuhara <uYasuhara@cade1dd4-7bb4-cb43-9a97-402565d5d6a2>
2014-08-01 06:38:55 +0000

    git-svn-id: http://10.16.14.243:13000/svn/azbIvr@73 cade1dd4-7bb4-cb43-9a97-402565d5d6a2

---
+++ trunk/IvrSource/ハンドラー増殖ツール/HandlerPatch/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' アセンブリに関する一般情報は以下の属性セットをとおして制御されます。 
+' アセンブリに関連付けられている情報を変更するには、
+' これらの属性値を変更してください。
+
+' アセンブリ属性の値を確認します。
+
+<Assembly: AssemblyTitle("HandlerPatch")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("（株）電通国際情報サービス")> 
+<Assembly: AssemblyProduct("HandlerPatch")> 
+<Assembly: AssemblyCopyright("Copyright (C) （株）電通国際情報サービス 2012")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'このプロジェクトが COM に公開される場合、次の GUID がタイプ ライブラリの ID になります。
+<Assembly: Guid("417b7572-b84b-4627-8fd1-73d21e7a64e3")> 
+
+' アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
+' 既定値にすることができます:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
